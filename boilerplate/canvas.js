a22c0a36d668133477682f647aea6a0294450978
Vladimir Bykov <broadsw.rd@yandex.ru>
2017-05-09 10:11:58 +0300

    Boilerplate

---
+++ boilerplate/canvas.js
@@ -0,0 +1,23 @@
+/*
+global Renderium Animation Vector
+*/
+
+var raf = window.requestAnimationFrame
+
+raf(function loop (time) {
+  Renderium.digest(time)
+  Animation.animate(time)
+  raf(loop)
+})
+
+var renderer = window.renderer = new Renderium({
+  el: document.getElementById('root')
+})
+
+var layer = window.layer = new Renderium.CanvasLayer({
+  Vector
+})
+
+renderer.addLayer(layer)
+
+Renderium.spawn(renderer)
