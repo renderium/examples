8db45d0b120796d7c9ccce9400000cc4c541c220
Vladimir Bykov <broadsw.rd@yandex.ru>
2017-06-28 10:38:10 +0300

    Improved repl circles examples

--- repl/circles.js
+++ repl/circles.js
@@ -1,5 +1,5 @@
 /*
-global Renderium Animation Vector layer
+global Renderium Vector layer
 */
 
 class Circle extends Renderium.Component {
@@ -10,91 +10,80 @@ class Circle extends Renderium.Component {
     this.fillColor = options.fillColor
     this.radius = options.radius
     this.width = options.width
-    this.duration = options.duration
+    this.opacity = options.opacity
+    this.lineDash = options.lineDash
 
-    this.layerWidth = 0
-
-    this._position = this.position.copy()
     this._shouldRedraw = true
-
-    this.animation = new Animation({
-      duration: this.duration,
-      handler: this._hanlder.bind(this)
-    })
-    this.animation.queue(this.animation)
   }
 
   shouldRedraw () {
     return this._shouldRedraw
   }
 
-  onadd (layer) {
-    this.animation.start()
-  }
-
-  onremove (layer) {
-    this.animation.cancel()
-  }
-
-  plot (layer, time) {
-    this.layerWidth = layer.width
-    this.animation.animate(time)
-  }
-
   draw (layer) {
     layer.drawCircle({
-      position: this._position,
+      position: this.position,
       color: this.color,
       fillColor: this.fillColor,
       radius: this.radius,
-      width: this.width
+      width: this.width,
+      opacity: this.opacity,
+      lineDash: this.lineDash
     })
 
     this._shouldRedraw = false
-  }
-
-  _hanlder (t) {
-    this._position.x = (this.position.x + this.layerWidth * t) % this.layerWidth
-    this._shouldRedraw = true
   }
 }
 
 layer.addComponents([
   new Circle({
-    position: new Vector(100, 100),
+    position: new Vector(75, 75),
     color: Renderium.colors.RED,
     fillColor: void 0,
     radius: 25,
     width: 1,
-    duration: 3000
+    opacity: 1,
+    lineDash: []
   }),
   new Circle({
-    position: new Vector(225, 100),
+    position: new Vector(175, 75),
     color: void 0,
     fillColor: layer.createGradient({
-      start: new Vector(0, 75),
-      end: new Vector(0, 125),
+      start: new Vector(0, 50),
+      end: new Vector(0, 100),
       from: Renderium.colors.LIGHT_BLUE,
       to: Renderium.colors.INDIGO
     }),
     radius: 25,
     width: 2,
-    duration: 3000
+    opacity: 1,
+    lineDash: []
   }),
   new Circle({
-    position: new Vector(350, 100),
-    color: Renderium.colors.GREEN,
-    fillColor: void 0,
-    radius: 25,
-    width: 5,
-    duration: 3000
-  }),
-  new Circle({
-    position: new Vector(475, 100),
+    position: new Vector(300, 75),
     color: Renderium.colors.YELLOW,
     fillColor: void 0,
     radius: 50,
     width: 2,
-    duration: 3000
+    opacity: 1,
+    lineDash: []
+  }),
+  new Circle({
+    position: new Vector(425, 75),
+    color: Renderium.colors.GREEN,
+    fillColor: void 0,
+    radius: 25,
+    width: 5,
+    opacity: 0.5,
+    lineDash: []
+  }),
+  new Circle({
+    position: new Vector(525, 75),
+    color: Renderium.colors.PURPLE,
+    fillColor: void 0,
+    radius: 25,
+    width: 5,
+    opacity: 1,
+    lineDash: [10, 5]
   })
 ])
